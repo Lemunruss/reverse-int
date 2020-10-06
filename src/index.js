95aad271c275518bdfb9ac7ced37141f4d906db1
Igor <lemunruss1@gmail.com>
2020-10-06 19:07:51 +0300

    init: start Reverse Int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = -n;
+    }
+  let str = String(n);
+  let arr = str.split('');
+  let arr2 = arr.reverse();
+  str = arr2.join('');
+  n = +str
+  return n;
 }
